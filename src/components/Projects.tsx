d3c11c4520997cc87ac02c32474b702cb0b81776
okech-michael <204019715+okech-michael@users.noreply.github.com>
2026-07-13 14:54:38 +0300

    Add SMACOM screenshot and fix MC Maticha project image

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -165,7 +165,7 @@ const Projects: React.FC = () => {
 
           <div className="featured-card" data-animate="fade-up">
             <div className="featured-card-preview">
-              <img src="/images/mc-maticha-website.svg" alt="MC Maticha Events" className="project-screenshot" />
+              <img src="/images/mc-maticha-website.jpg" alt="MC Maticha Events" className="project-screenshot" />
             </div>
             <div className="featured-card-info">
               <span className="project-label">Featured · Events</span>
@@ -231,21 +231,9 @@ const Projects: React.FC = () => {
             </div>
           </div>
 
-          {/* SMACOM - NO SCREENSHOT */}
           <div className="featured-card featured-card-reverse" data-animate="fade-up">
             <div className="featured-card-preview">
-              <div className="preview-browser">
-                <div className="browser-bar">
-                  <span className="dot red"></span>
-                  <span className="dot yellow"></span>
-                  <span className="dot green"></span>
-                  <span className="browser-url">smacom.co.ke</span>
-                </div>
-                <div className="preview-content smart-preview">
-                  <div className="preview-icon">📱</div>
-                  <p>Smart Community Platform</p>
-                </div>
-              </div>
+              <img src="/images/smacom-site.jpg" alt="SMACOM Platform" className="project-screenshot" />
             </div>
             <div className="featured-card-info">
               <span className="project-label">Featured · IoT & Smart Systems</span>
